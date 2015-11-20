38bd22934f910b2426a7c0a65d76d51e73a38efd
Shana Azria <shanaazria2016@u.northwestern.edu>
2015-11-19 19:57:23 -0600

    Initial Commit

---
+++ AdherenceApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdherenceApp
+//
+//  Created by Shana Azria Dev on 11/19/15.
+//  Copyright © 2015 Shana Azria Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AdherenceApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdherenceApp
+//
+//  Created by Shana Azria Dev on 11/19/15.
+//  Copyright © 2015 Shana Azria Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
